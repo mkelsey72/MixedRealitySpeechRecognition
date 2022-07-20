73b5223d9546883446c7225996864c617066057a
kels <105393865+kelseymd@users.noreply.github.com>
2022-07-20 18:44:51 -0500

    toggle mode works, but transparent

--- Assets/LightDarkMode.cs
+++ Assets/LightDarkMode.cs
@@ -5,7 +5,7 @@ using TMPro;
 
 public class LightDarkMode : MonoBehaviour
 {
-    private static Color DARK_COLOR = Color.grey;
+    private static Color DARK_COLOR = new Color(.2f,.2f,.2f,1); //new Color(35.00f/255.0f, 35.00f/255.00f, 35.00f/255.00f);
     private static Color LIGHT_COLOR = Color.white;
 
     private TextMeshProUGUI subtitleText;
@@ -13,14 +13,21 @@ public class LightDarkMode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.subtitleText = this.GetComponent<TextMeshProUGUI>();    
+        this.subtitleText = this.GetComponent<TextMeshProUGUI>();
+        this.subtitleText.color = Color.white;
     }
 
     public void changeColor()
     {
         if (subtitleText.color == LIGHT_COLOR)
+        {
+            subtitleText.faceColor = DARK_COLOR;
             subtitleText.color = DARK_COLOR;
+        }
         else
+        {
+            subtitleText.faceColor = LIGHT_COLOR;
             subtitleText.color = LIGHT_COLOR;
+        }
     }
 }
